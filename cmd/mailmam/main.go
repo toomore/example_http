33fa9e7e5995018686cfda64e772a1970803e352
Toomore Chiang <toomore0929@gmail.com>
2015-07-05 00:55:18 +0800

    Add GOMAXPROCS.

--- cmd/mailmam/main.go
+++ cmd/mailmam/main.go
@@ -13,20 +13,26 @@ import (
 	"github.com/toomore/simpleaws/utils"
 )
 
-var AWSID = flag.String("awsid", os.Getenv("AWSID"), "AWSID")
-var AWSKEY = flag.String("awskey", os.Getenv("AWSKEY"), "AWSKEY")
-var SQSRegion = flag.String("sqsregion", "", "AWS SQS region")
-var SQSURL = flag.String("sqsurl", "", "AWS SQS queue URL")
-var SQSReceiverMax = flag.Int64("sqsrecmax", 10, "AWS SQS receiver max")
-var retry = flag.Int64("retry", 5, "Get queue in zero to retry times")
-var sqsObject *sqs.SQS
+var (
+	AWSID          = flag.String("awsid", os.Getenv("AWSID"), "AWSID")
+	AWSKEY         = flag.String("awskey", os.Getenv("AWSKEY"), "AWSKEY")
+	SQSReceiverMax = flag.Int64("sqsrecmax", 10, "AWS SQS receiver max")
+	SQSRegion      = flag.String("sqsregion", "", "AWS SQS region")
+	SQSURL         = flag.String("sqsurl", "", "AWS SQS queue URL")
+	ncpu           = flag.Int("ncpu", runtime.NumCPU(), "指定 CPU 數量，預設為實際 CPU 數量")
+	retry          = flag.Int64("retry", 5, "Get queue in zero to retry times")
+	sqsObject      *sqs.SQS
+)
 
 func getQmsg(rmax int64) {
-	var zerotimes int64
-	var delta int64
+	var (
+		delta     int64
+		wg        sync.WaitGroup
+		zerotimes int64
+	)
+
 Send:
 	if msg, err := sqsObject.Receive(rmax); err == nil {
-		var wg sync.WaitGroup
 		wg.Add(len(msg.Messages))
 		for i, m := range msg.Messages {
 			// Decode base64, ParseQuery
@@ -60,6 +66,9 @@ Send:
 
 func main() {
 	flag.Parse()
+
+	runtime.GOMAXPROCS(*ncpu)
+
 	if *AWSID == "" || *AWSID == "" {
 		log.Fatal("Lost AWSID or AWSKEY")
 	}
