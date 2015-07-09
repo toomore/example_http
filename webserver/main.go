b7fb5e23c066925fc62e47b17c8887db02775b26
Toomore Chiang <toomore0929@gmail.com>
2015-07-09 14:36:30 +0800

    Move http.HandleiFunc to view

--- webserver/main.go
+++ webserver/main.go
@@ -2,34 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/toomore/example_http/webserver/config"
-	"github.com/toomore/example_http/webserver/session"
 	"github.com/toomore/example_http/webserver/view"
 )
 
-func needLogin(httpFunc func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, resp *http.Request) {
-		log.Println("In wrapper", resp.UserAgent())
-		var Session = session.New(config.SESSIONKEY, w, resp)
-		log.Printf(">>>[%s] %+v", Session.Get("user"), Session.Hashvalues)
-		if Session.Get("user") == "" {
-			http.Redirect(w, resp, "/", http.StatusTemporaryRedirect)
-		}
-		httpFunc(w, resp)
-	}
-}
-
 func main() {
 	log.Println("Hello Toomore")
-
-	http.HandleFunc("/", view.Index)
-	http.HandleFunc("/login", view.Login)
-	http.HandleFunc("/board", needLogin(view.Board))
-	http.HandleFunc("/sendmail", needLogin(view.Sendmail))
-
-	if err := http.ListenAndServe(":59122", nil); err != nil {
-		log.Fatal(err)
-	}
+	view.Run(":59122")
 }
--- webserver/view/base.go
+++ webserver/view/base.go
@@ -1,6 +1,13 @@
 package view
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/toomore/example_http/webserver/config"
+	"github.com/toomore/example_http/webserver/session"
+)
 
 var tpl map[string]*template.Template
 
@@ -10,4 +17,27 @@ type outputdata struct {
 
 func init() {
 	tpl = make(map[string]*template.Template)
+}
+
+func NeedLogin(httpFunc func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, resp *http.Request) {
+		log.Println("In wrapper", resp.UserAgent())
+		var Session = session.New(config.SESSIONKEY, w, resp)
+		log.Printf(">>>[%s] %+v", Session.Get("user"), Session.Hashvalues)
+		if Session.Get("user") == "" {
+			http.Redirect(w, resp, "/", http.StatusTemporaryRedirect)
+		}
+		httpFunc(w, resp)
+	}
+}
+
+func Run(address string) {
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/board", NeedLogin(Board))
+	http.HandleFunc("/sendmail", NeedLogin(Sendmail))
+
+	if err := http.ListenAndServe(address, nil); err != nil {
+		log.Fatal(err)
+	}
 }
